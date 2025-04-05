1eecf00a74e9dbe6bfb2f8c2e5b789900d10e8ff
jabbarrr7 <125061912+jabbarrr7@users.noreply.github.com>
2025-04-05 19:36:03 +0700

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import uuid
+
+def parse_csv_input(uploaded_file):
+    df = pd.read_csv(uploaded_file)
+    tasks = df["task"].tolist()
+    teachers = df["teacher"].unique().tolist()
+    rooms = df["room"].unique().tolist()
+    timeslots = df["timeslot"].unique().tolist()
+    return tasks, teachers, rooms, timeslots
+
+def save_schedule_as_csv(schedule):
+    df = pd.DataFrame(schedule)
+    filename = f"jadwal_{uuid.uuid4().hex[:6]}.csv"
+    df.to_csv(filename, index=False)
