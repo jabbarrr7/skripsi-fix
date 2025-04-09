020761527c51e4d03974d6a4f5090231d4df6f86
jabbarrr7 <125061912+jabbarrr7@users.noreply.github.com>
2025-04-09 11:57:34 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,11 +3,12 @@ import uuid
 
 def parse_csv_input(uploaded_file):
     df = pd.read_csv(uploaded_file)
-    tasks = df["task"].tolist()
+    tasks = df[["task", "teacher"]].to_dict(orient="records")  # ⬅️ Ini yang berubah
     teachers = df["teacher"].unique().tolist()
     rooms = df["room"].unique().tolist()
     timeslots = df["timeslot"].unique().tolist()
     return tasks, teachers, rooms, timeslots
+
 
 def save_schedule_as_csv(schedule):
     df = pd.DataFrame(schedule)
