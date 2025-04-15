f089cb5004606de0399785c12366e7b70714a152
jabbarrr7 <125061912+jabbarrr7@users.noreply.github.com>
2025-04-16 02:14:17 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import uuid
 
+
 def parse_csv_input(uploaded_file):
     df = pd.read_csv(uploaded_file)
     # Pastikan tipe data 'sks' dan 'semester' diubah ke integer
@@ -13,7 +14,7 @@ def parse_csv_input(uploaded_file):
     
     # Buat list timeslot berupa angka. Misalnya, kita buat 12 slot per hari.
     # Untuk contoh, kita gunakan angka 1 s.d. 12.
-    timeslots = list(range(1, 13))
+    timeslots = list(df['timeslot'].unique())
     
     # Semester juga diambil dari data (dalam hal ini, 1 sampai 7)
     semesters = sorted(df["semester"].unique().tolist())
