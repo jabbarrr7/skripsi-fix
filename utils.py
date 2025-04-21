9396a98b223672c52d22047aa63d5ace7d3b24cc
jabbarrr7 <125061912+jabbarrr7@users.noreply.github.com>
2025-04-21 19:44:31 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import uuid
+from datetime import datetime, timedelta
 
 
 def parse_csv_input(uploaded_file):
@@ -23,6 +24,13 @@ def parse_csv_input(uploaded_file):
     classes = ['A', 'B', 'C', 'D']
     
     return courses, teachers, rooms, timeslots, semesters, classes
+def parse_timeslot(timeslot_str):
+    day, time_range = timeslot_str.split(', ')
+    start_str, end_str = time_range.split('-')
+    start_time = datetime.strptime(start_str, "%H:%M")
+    end_time = datetime.strptime(end_str, "%H:%M")
+    return day.strip(), start_time, end_time
+
 
 def save_schedule_as_csv(schedule, filename_prefix="jadwal"):
     df = pd.DataFrame(schedule)
